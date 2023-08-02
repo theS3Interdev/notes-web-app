4a8b4a39b3c92e773b139090330b04b05092cf49
Michael Owuor <michael.owuor@gmail.com>
2023-08-02 15:45:16 +0300

    feat: new note form

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,21 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { NewNote } from '@/components';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 	return (
-		<div className="container mx-auto my-4">
+		<Container className="my-4">
 			<Routes>
 				<Route path="/" element={<h1>Home</h1>} />
-				<Route path="/new" element={<h1>New</h1>} />
+				<Route path="/new" element={<NewNote />} />
 				<Route path="/:id">
 					<Route index element={<h1>Show</h1>} />
 					<Route path="edit" element={<h1>Edit</h1>} />
 				</Route>
 				<Route path="*" element={<Navigate to="/" />} />
 			</Routes>
-		</div>
+		</Container>
 	);
 }
 
